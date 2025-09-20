0315918b0952db3236fffd900d705c7c4f5f48a8
휘래 김 <hramst0618@gmail.com>
2025-09-21 03:07:24 +0900

    feat: simple pyspark 프로그램 작성

--- pyspark/simple_pyspark.py
+++ pyspark/simple_pyspark.py
@@ -7,7 +7,7 @@ spark = SparkSession \
     .getOrCreate()
 
 schema = 'NAME STRING, AGE INT, MARRIAGE BOOLEAN'
-df = spark.createDataFrame(data=[('KIM', 25), ('MIN', 22)], schema=schema)
+df = spark.createDataFrame(data=[('KIM', 25, True), ('MIN', 22, False)], schema=schema)
 df.show()
 
 # sleep 5 minutes
